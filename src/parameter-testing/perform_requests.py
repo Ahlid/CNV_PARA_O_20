c17f973623cbc5a82cb23da7e9072fad27b62632
Luis Santos <luis@paiva.eu>
2018-04-21 14:03:19 +0100

    increased parameter testing coverage for Maze50

--- src/parameter-testing/perform_requests.py
+++ src/parameter-testing/perform_requests.py
@@ -14,3 +14,4 @@ for i, params in enumerate(parameter_list):
     r = requests.get(request_url, params)
     elapsed_secs = r.elapsed.total_seconds()
     print('Request {} took {}s'.format(i+1, elapsed_secs))
+    print(r.url)
---
+++ src/parameter-testing/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -f ../../results.csv
+python perform_requests.py
