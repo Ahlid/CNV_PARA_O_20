aa87e051e5c5eb5cc1367fc500e60b66946bc259
Dario Ornelas <ornelas.dario@gmail.com>
2018-04-22 21:04:42 +0100

    added initial struct for loadbalancer, auto scaler and proxied server to forward requests to workers

---
+++ src/pt/ulisboa/tecnico/meic/cnv/loadbalancer/LoadBalancer.java
@@ -0,0 +1,57 @@
+package pt.ulisboa.tecnico.meic.cnv.loadbalancer;
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LoadBalancer implements Balancer {
+    final static Logger logger = Logger.getLogger(LoadBalancer.class);
+
+    protected List<WorkerInstance> workers;
+    protected Scaler scaler;
+
+    public LoadBalancer() throws Exception{
+        logger.info("Initializing Balancer...");
+        workers = new ArrayList<>();
+
+    }
+
+    public void setScaler(Scaler scaler) { this.scaler = scaler; }
+
+    public WorkerInstance getInstance(){
+        Double cpu = 100.0;
+        Double progress = 0.0;
+        Integer size = 150;
+        WorkerInstance faster=null;
+        workers = scaler.getWorkers();
+
+
+
+        for (WorkerInstance w : workers){
+            System.out.println("[lb]:id : " + w.getId() + ", cpu use: " + w.getCPU() + ", progress: " + w.getProgress() + ", size: " + w.getSize() + ", working: " + w.working());
+            if (!w.working() || (w.getCPU() < cpu) && (w.getProgress() > 0.50) || w.getSize() == 0 ){
+                cpu = w.getCPU();
+                progress = w.getProgress();
+                faster = w;
+                System.out.println("hello world!");
+            }
+            else { return workers.get(0); }
+        }
+
+        return workers.get(workers.indexOf(faster));
+    }
+
+    @Override
+    public synchronized void addWorkerBalancer(WorkerInstance instance) {
+        logger.info("Remove worker " + instance.getId());
+        workers.remove(instance);
+    }
+
+    @Override
+    public synchronized void removeWorkerBalancer(WorkerInstance instance) {
+        logger.info("Add worker " + instance.getId());
+        workers.add(instance);
+    }
+
+}
---
+++ src/pt/ulisboa/tecnico/meic/cnv/loadbalancer/Proxy.java
@@ -0,0 +1,65 @@
+package pt.ulisboa.tecnico.meic.cnv.loadbalancer;
+
+import java.net.InetSocketAddress;
+import java.util.Timer;
+import java.util.concurrent.Executors;
+
+import com.sun.net.httpserver.HttpServer;
+import org.apache.log4j.Logger;
+
+
+public class Proxy {
+    final static Logger logger = Logger.getLogger(Proxy.class);
+
+    private HttpServer proxiedServer = null;
+    private static int PROXY_PORT = 8080;
+
+    private static Balancer balancer = null;
+    private static Scaler scaler = null;
+
+    public Proxy() throws Exception {
+        balancer = new LoadBalancer();
+        scaler = new Scaler(balancer);
+        balancer.setScaler(scaler);
+
+    }
+
+
+    public static void main(String[] args) {
+
+        try {
+
+            Proxy proxy = new Proxy();
+            proxy.start();
+
+        } catch (Exception e) {
+            System.out.println("oops at proxy: " + e.getMessage());
+        }
+    }
+
+
+    private void terminate() throws Exception {
+        logger.info("Stopping Proxy...");
+        scaler.setState(false);
+        scaler.join();
+        proxiedServer.stop(1);
+        logger.info("Proxy Stopped.");
+    }
+
+    public void start() throws Exception {
+        // logger.info("Launching Scaler...");
+        scaler.start();
+
+        logger.info("Launching Proxy at port " + PROXY_PORT);
+        proxiedServer = HttpServer.create(new InetSocketAddress(PROXY_PORT), 0);
+        proxiedServer.createContext("/", new HandleRequest(scaler, balancer));
+        proxiedServer.setExecutor(Executors.newCachedThreadPool());
+        proxiedServer.start();
+
+        logger.info("Proxy started.");
+
+        // And From your main() method or any other method
+        Timer timer = new Timer();
+        timer.schedule(scaler.sayHello, 0, 30000);
+    }
+}
