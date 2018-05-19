cba1cb854976b63c00641225486227965faf6718
EC2 Default User <ec2-user@ip-172-31-95-33.ec2.internal>
2018-05-19 13:16:03 +0000

    adding scripts to update worker and balancer AMIs

---
+++ scripts/update-balancer-ami.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ~ &&
+INSTANCE_ID=$(ec2-metadata --instance-id | cut -d ' ' -f2) &&
+
+echo "Recreating AMI: balancer-ami"
+OLD_BALANCER_AMI_ID=$(aws ec2 describe-images --owners self --filters Name=name,Values=balancer-ami | grep -o "ami-[a-zA-Z0-9]*")
+[ -n $OLD_BALANCER_AMI_ID ] && aws ec2 deregister-image --image-id $OLD_BALANCER_AMI_ID
+
+BALANCER_AMI_ID=$(aws ec2 create-image --instance-id $INSTANCE_ID --no-reboot --name balancer-ami) &&
+aws ec2 wait image-available --image-ids $BALANCER_AMI_ID &&
+echo "Balancer AMI Id: $BALANCER_AMI_ID"
+
+echo Done
---
+++ scripts/update-worker-ami.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ~ &&
+INSTANCE_ID=$(ec2-metadata --instance-id | cut -d ' ' -f2) &&
+
+echo "Recreating AMI: worker-ami"
+OLD_WORKER_AMI_ID=$(aws ec2 describe-images --owners self --filters Name=name,Values=worker-ami | grep -o "ami-[a-zA-Z0-9]*")
+[ -n $OLD_WORKER_AMI_ID ] && aws ec2 deregister-image --image-id $OLD_WORKER_AMI_ID
+
+WORKER_AMI_ID=$(aws ec2 create-image --instance-id $INSTANCE_ID --no-reboot --name worker-ami) &&
+aws ec2 wait image-available --image-ids $WORKER_AMI_ID &&
+echo "Worker AMI Id: $WORKER_AMI_ID"
+
+echo "Updating Worker AMI on DynamoDB"
+cd CNV_PARA_O_20/ && make updateami name=$WORKER_AMI_ID &> /dev/null && cd .. &&
+echo Done
