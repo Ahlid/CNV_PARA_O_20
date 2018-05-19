83a1de9b226924e9c78fa4e5b6f511741eaf0ce9
EC2 Default User <ec2-user@ip-172-31-88-57.ec2.internal>
2018-05-19 19:44:36 +0000

    creating load balancer instance with balancer SG on setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 cd ~ &&
-echo ===================================
-echo = Installing Java SDK and Git     =   
-echo ===================================
+echo =======================================
+echo = Installing Java SDK and Git         =
+echo =======================================
 sudo yum -y install java-1.7.0-devel git &&
-echo ===================================
-echo = Fetching and unzip AWS SDK      =   
-echo ===================================
+echo =======================================
+echo = Fetching and unzipping AWS SDK      =
+echo =======================================
 wget http://sdk-for-java.amazonwebservices.com/latest/aws-java-sdk.zip &&
 unzip aws-java-sdk.zip &&
 rm aws-java-sdk.zip &&
-echo ===================================
-echo = Cloning Git Repo                =   
-echo ===================================
+echo =======================================
+echo = Cloning Git Repo                    =
+echo =======================================
 git clone https://github.com/Ahlid/CNV_PARA_O_20.git &&
-echo ===================================
-echo = Compiling project               =   
-echo ===================================
+echo =======================================
+echo = Compiling project                   =
+echo =======================================
 AWS_SDK_DIR=$(echo aws-java-sdk-*)
 AWS_SDK_VERSION=${AWS_SDK_DIR#aws-java-sdk-}
 sed -i "s/AWS_VERSION=.*/AWS_VERSION=$AWS_SDK_VERSION/" ~/CNV_PARA_O_20/Makefile &&
 cd CNV_PARA_O_20/ && make all && make run_inst && cd .. &&
-echo ===================================
-echo = Creating AWS Credentials        =   
-echo ===================================
+echo =======================================
+echo = Creating AWS Credentials            =
+echo =======================================
 echo "Access Key:"
 read key &&
 echo "Secret Access Key:"
@@ -36,9 +36,9 @@ echo aws_secret_access_key=$secret >> ~/.aws/credentials &&
 echo [default] >> ~/.aws/config &&
 echo region=us-east-1 >> ~/.aws/config &&
 echo output=text >> ~/.aws/config &&
-echo =====================================
-echo = Creating worker and balancer AMIs =   
-echo =====================================
+echo =======================================
+echo = Creating worker and balancer AMIs   =
+echo =======================================
 INSTANCE_ID=$(ec2-metadata --instance-id | cut -d ' ' -f2) &&
 echo "Creating AMI: worker-ami"
 sudo cp ~/CNV_PARA_O_20/scripts/rc.local.worker /etc/rc.local && sync &&
@@ -55,9 +55,9 @@ echo "Balancer AMI Id: $BALANCER_AMI_ID"
 echo "Updating Worker AMI on DynamoDB"
 cd CNV_PARA_O_20/ && make updateami name=$WORKER_AMI_ID &> /dev/null && cd .. &&
 
-echo =====================================
-echo = Creating worker and balancer SGs =   
-echo =====================================
+echo =======================================
+echo = Creating worker and balancer SGs    =
+echo =======================================
 echo "Creating Security Group: CNV-worker-sg"
 aws ec2 create-security-group --description "Allows SSH + HTTP at a worker instance" --group-name CNV-worker-sg
 aws ec2 authorize-security-group-ingress --group-name CNV-worker-sg --protocol tcp --port 8000 --cidr 0.0.0.0/0
@@ -68,7 +68,12 @@ aws ec2 create-security-group --description "Allows SSH + HTTP at the load balan
 aws ec2 authorize-security-group-ingress --group-name CNV-balancer-sg --protocol tcp --port 80 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-name CNV-balancer-sg --protocol tcp --port 22 --cidr 0.0.0.0/0
 
+echo =======================================
+echo = Launching a load balancer instance  =
+echo =======================================
+aws ec2 run-instances --image-id $BALANCER_AMI_ID --count 1 --instance-type t2.micro --security-groups CNV-balancer-sg &> /dev/null &&
+
 echo Done
 echo "Terminating current instance as it is no longer needed"
-echo "You may now manually create an instance with the AMI: balancer-ami"
+echo "You may stop the load balancer instance anytime to stop incurring costs"
 aws ec2 terminate-instances --instance-ids $INSTANCE_ID &> /dev/null
